7b5fecd5eec6ef915dbd59dc62410ad88bdce52a
RomanRavlyk <roman.ravlyk.ri.2023@lpnu.ua>
2025-06-20 00:10:43 +0300

    Btree

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.prev = None
+        self.left = None
+        self.right = None
 
     def get_data(self):
         return self.data
@@ -9,207 +10,100 @@ class Node:
     def __repr__(self):
         return str(self.data)
 
-class Stack:
+class BinaryTree:
     def __init__(self):
-        self.tail = None
+        self.root = None
 
-    def push(self, node):
-        if self.tail is None:
-            self.tail = node
-            return True
+    def insert(self, data):
+        def _insert(root, data):
+            if root is None:
+                return Node(data)
 
-        node.prev = self.tail
-        self.tail = node
-        return True
+            if data < root.data:
+                root.left = _insert(root.left, data)
+            if data > root.data:
+                root.right = _insert(root.right, data)
+            return root
 
-    def pop(self):
-        if self.tail is None:
-            return None
-        if self.tail.prev is None:
-            data = self.tail.data
-            self.tail = None
-            return data
+        self.root = _insert(self.root, data)
 
-        data = self.tail.data
-        self.tail = self.tail.prev
-        return data
+    def find_min_value_node(self, node):
+        current = node
+        while current.left:
+            current = current.left
+        return current
 
 
-    # def enqueue_right(self, node):
-    #     if self.tail is None:
-    #         self.head = node
-    #         self.tail = node
-    #     else:
-    #         self.tail.next = node
-    #         node.prev = self.tail
-    #         self.tail = node
-    #
-    #     return True
-    #
-    # def enqueue_left(self, node):
-    #     if self.head is None:
-    #         self.head = node
-    #         self.tail = node
-    #     else:
-    #         self.head.prev = node
-    #         node.next = self.head
-    #         self.head = node
-    #
-    #     return True
-    #
-    # def dequeue_left(self):
-    #     if self.head is None:
-    #         return None
-    #
-    #     data = self.head.data
-    #     if self.head.next:
-    #         self.head = self.head.next
-    #         self.head.prev = None
-    #     else:
-    #         self.head = None
-    #     return data
-    #
-    # def dequeue_right(self):
-    #     if self.tail is None:
-    #         return None
-    #
-    #     data = self.tail.data
-    #     if self.tail.prev:
-    #         self.tail = self.tail.prev
-    #         self.tail.next = None
-    #     else:
-    #         self.tail = None
-    #     return data
-    #
-    def peek(self):
-        if self.tail is None:
-            return None
+    def delete(self, data):
+        def _delete(root, data):
+            if root is None:
+                return root
 
-        return self.tail
+            if data < root.data:
+                root.left = _delete(root.left, data)
+            elif data > root.data:
+                root.right = _delete(root.right, data)
+            else:
+                if root.left is None:
+                    return root.right
+                if root.right is None:
+                    return root.left
 
-    def get_linked_list(self):
-        current = self.tail
-        while current:
-            print(current)
-            current = current.prev
+                temp = self.find_min_value_node(root.right)
+                root.data = temp.data
+                root.right = _delete(root.right, temp.data)
 
-    def is_empty(self):
-        return self.tail is None
+            return root
 
-    # def get_linked_list_backward(self):
-    #     current = self.head
-    #     while current.next:
-    #         current = current.next
-    #
-    #     while current:
-    #         print(current)
-    #         current = current.prev
+        self.root = _delete(self.root, data)
+
+    def height(self, node):
+        if node is None:
+            return 0
+
+        left_height = self.height(node.left)
+        right_height = self.height(node.right)
+        print(f"Node {node.data}: left = {left_height}, right = {right_height}")
+        return 1 + max(left_height, right_height)
 
 
-    # def insert(self, node: Node, position: int):
-    #
-    #     if position == 0:
-    #         node.next = self.head
-    #         self.head = node
-    #
-    #     current = self.head
-    #     prev = None
-    #     count = 0
-    #
-    #     while current and count < position:
-    #         prev = current
-    #         current = current.next
-    #         count += 1
-    #
-    #     prev.next = node
-    #     node.next = current
-    #
-    # def pop(self):
-    #     if self.head is None:
-    #         return False
-    #     if not self.head.next:
-    #         data = self.head.data
-    #         self.head = None
-    #         return data
-    #
-    #     last = self.head.prev
-    #     data = last.data
-    #     last_prev = last.prev
-    #     last_prev.next = self.head
-    #     self.head.prev = last_prev
-    #
-    #     return data
-    #
-    # def popfirst(self):
-    #     if self.head is None:
-    #         return False
-    #
-    #     current = self.head
-    #     while current.prev:
-    #         current = current.prev
-    #
-    #     data = current.data
-    #
-    #     if current.next:
-    #         current.next.prev = None
-    #     else:
-    #         self.head = None
-    #         return data
-    #
-    #     self.head = current.next
-    #     return data
-    #
-    # def remove(self, value):
-    #     if self.head is None:
-    #         return None
-    #     if self.head.data == value:
-    #         self.head = self.head.next
-    #         return True
-    #     current = self.head
-    #     prev = None
-    #
-    #     while current:
-    #         if current.data == value:
-    #             prev.next = current.next
-    #             return True
-    #         prev = current
-    #         current = current.next
-    #     return False
-    #
-    # def is_empty(self):
-    #     if self.head is None:
-    #         return True
-    #     else:
-    #         return False
-    #
-    # def search(self, value):
-    #     current = self.head
-    #     while current:
-    #         if current.data == value:
-    #             return current
-    #         current = current.next
-    #     return None
 
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
-node4 = Node(4)
+    def inorder(self):
+        def _inorder(node):
+            if node:
+                _inorder(node.left)
+                print(node.data, end=" ")
+                _inorder(node.right)
 
-linkedlist = Stack()
-# linkedlist.enqueue_right(node1)
-# linkedlist.enqueue_right(node2)
-# linkedlist.enqueue_right(node3)
-# linkedlist.enqueue_right(node4)
-# linkedlist.dequeue()
-linkedlist.push(node1)
-linkedlist.push(node2)
-linkedlist.push(node3)
-linkedlist.push(node4)
-linkedlist.get_linked_list()
-linkedlist.pop()
-linkedlist.get_linked_list()
-# print(f"peek: {linkedlist.peek()}")
-# # linkedlist.pop()
-# # linkedlist.remove(3)
-# # linkedlist.get_linked_list()
-# print(linkedlist.search(3))
+        _inorder(self.root)
+        print()
+
+    def preorder(self):
+        def _preorder(node):
+            if node:
+                print(node.data,  end=" ")
+                _preorder(node.left)
+                _preorder(node.right)
+        _preorder(self.root)
+        print()
+
+    def postorder(self):
+        def _postorder(node):
+            if node:
+                _postorder(node.left)
+                _postorder(node.right)
+                print(node.data, end=" ")
+        _postorder(self.root)
+        print()
+
+btree = BinaryTree()
+
+btree.insert(2)
+btree.insert(1)
+btree.insert(3)
+btree.inorder()
+btree.preorder()
+btree.postorder()
+btree.delete(3)
+btree.inorder()
+print(btree.height(btree.root))
